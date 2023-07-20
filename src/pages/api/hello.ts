c734259c72b0403145200ba5911134837abaf77a
Dimitris Ragkousis <d.ragkousis@gmail.com>
2023-07-20 22:12:13 +0300

    Prisma sample

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import prisma from '../../../lib/prisma'
+import { TYPE } from '@prisma/client'
 type Data = {
-  name: string
+    name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    const feed = await prisma.event.create({
+        data: {
+            name: 'Prisma Day',
+            description:
+                'Prisma Day is a full day of talks and networking centered around the Prisma ecosystem.',
+            slug: 'lasdsa',
+            date: new Date(),
+            reminderFrom: new Date(),
+            type: TYPE.event,
+        },
+    })
+
+    res.status(200).json(feed)
 }
